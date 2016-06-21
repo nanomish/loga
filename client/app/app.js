763c0dda731ba2dcb3e7f9cec1cd64e7cf675539
Michael <michael.kushniriov@gmail.com>
2016-06-21 13:24:38 +0300

    added ecmascript via babel and start script

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,16 @@
 'use strict';
 
-angular.module('logaApp', ['logaApp.auth', 'logaApp.admin', 'logaApp.constants', 'ngCookies',
-    'ngResource', 'ngSanitize', 'ngRoute', 'btford.socket-io', 'ui.bootstrap', 'validation.match'
+angular.module('logaApp', [
+  'logaApp.auth',
+  'logaApp.admin',
+  'logaApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'btford.socket-io',
+  'ui.bootstrap',
+  'validation.match'
   ])
   .config(function($routeProvider, $locationProvider) {
     $routeProvider.otherwise({
